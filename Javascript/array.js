786bc0ecdac67a476d7b5a63cc4fa3fd47352513
Shitaalrai <raichichi50@gmail.com>
2026-08-22 08:12:48 +0545

    javascript array

---
+++ Javascript/array.js
@@ -0,0 +1,12 @@
+const arr1 = [1,2,3,4,7];
+const arr2 = [11,12,13,44,57];
+let arr3 = [];
+for (var i=0; i<arr1.length; i++)
+{
+    arr3[i]=arr1[i];
+}
+for (var i=0; i<arr2.length; i++)
+{
+    arr3[i + arr1.length]=arr2[i];
+}
+console.log(arr3);
