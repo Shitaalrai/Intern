4909198f1e392024f715050d7b971ff17adc7dac
Shitaalrai <raichichi50@gmail.com>
2026-08-23 13:19:49 +0545

    Dom Task

---
+++ Dom/ONOFF.js
@@ -0,0 +1,15 @@
+const onbtn = document.getElementById("btn1");
+const offbtn = document.getElementById("btn2");
+const Container = document.querySelector(".container");
+
+onbtn.addEventListener("click",()=> {
+    console.log(Container)
+    offbtn.style.display = "inline";
+    onbtn.style.display = "none";
+    Container.style.backgroundColor = "black";
+})
+offbtn.addEventListener("click", () => {
+    offbtn.style.display = "none";
+    onbtn.style.display = "inline";
+    Container.style.backgroundColor= "white";
+})
