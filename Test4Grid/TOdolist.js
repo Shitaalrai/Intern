b5b6c48b2c1267c25834752d23a8e270fc4b2bf1
Shitaalrai <raichichi50@gmail.com>
2026-08-25 14:07:27 +0545

    Tried Todo list

--- Test4Grid/TOdolist.js
+++ Test4Grid/TOdolist.js
@@ -4,9 +4,11 @@ const Addbtn = document.querySelector(".btn");
 const Submitbtn = document.querySelector(".btnsubmit");
 
 Addbtn.addEventListener("click", () => {
-    ContainerForm.style.display = "inline";
+    ContainerForm.style.display = "block";
+    Containerlist.style.display = "none";
 })
 
 Submitbtn.addEventListener("click", () => {
-    Containerlist.style.display = "inline";
+    Containerlist.style.display = "block";
+    ContainerForm.style.display = "none";
 })
