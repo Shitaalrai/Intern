8138d755e74b8c3c94f520c756add2dc87c12696
Shitaalrai <raichichi50@gmail.com>
2026-08-26 22:39:23 +0545

    Timer task improved

--- Dom/Timer.js
+++ Dom/Timer.js
@@ -1,20 +1,36 @@
-
-// let sec=0;
-// let min = 0;
-// let hrs = 0;
-// let stopwatch = setInterval(()=>{
-//     sec++;
-//     if(sec === 60){
-//         sec = 0;
-//         min++;
-//     }
-//     if (min === 60){
-//         min = 0;
-//         hrs++;
-//     }
-//     console.log("hrs:" +hrs,"min:" +min,"sec:" +sec);
-// },1000);
-
-const Hour = document.querySelector(".hour");
-const MIn = document.querySelector(".minute");
+const Startbtn = document.querySelector(".btn.start");
+const Resetbtn = document.querySelector(".btn.reset");
+const Pausebtn = document.querySelector(".btn.pause");
+const Hrs = document.querySelector(".hour");
+const Min = document.querySelector(".minute");
 const Sec = document.querySelector(".second");
+
+Startbtn.addEventListener("click", ()=>{
+    let sec = 0;
+    let min = 0;
+    let hrs = 0;
+    let stopwatch = setInterval(()=>{
+        sec++;
+        Sec.innerHTML = sec ;
+        console.log(sec);
+        if(sec === 60){
+            sec = 0;
+            min++;
+            Min.innerHTML = min;
+        }
+        if (min === 60){
+            min = 0;
+            hrs++;
+            Hrs.innerHTML = hrs;
+        }
+        // console.log("hrs:" +hrs,"min:" +min,"sec:" +sec);
+    },1000);
+});
+
+Resetbtn.addEventListener("click",()=>{
+    
+});
+Pausebtn.addEventListener("click",()=>{
+
+});
+
