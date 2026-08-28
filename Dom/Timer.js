06eec7dfd4f99a34c0a2c51011cb3e62421fac3f
Shitaalrai <raichichi50@gmail.com>
2026-08-28 21:10:40 +0545

    Resume button is added

--- Dom/Timer.js
+++ Dom/Timer.js
@@ -1,10 +1,17 @@
 const Startbtn = document.querySelector(".btn.start");
 const Resetbtn = document.querySelector(".btn.reset");
 const Pausebtn = document.querySelector(".btn.pause");
+const Resumebtn = document.querySelector(".btn.resume");
 const Hrs = document.querySelector(".hour");
 const Min = document.querySelector(".minute");
 const Sec = document.querySelector(".second");
 const Container = document.querySelector(".container");
+
+Resumebtn.addEventListener("click",()=>{
+  Resumebtn.style.display = "none";
+  Pausebtn.style.display = "inline";
+  Cal();
+})
 
 let sec = 0;
 let min = 0;
@@ -38,13 +45,16 @@ function stopinterval() {
   console.log("work");
 }
 
-function Reset() {}
-
 Startbtn.addEventListener("click", () => {
+  Startbtn.style.display = "none";
+  Pausebtn.style.display = "inline";
   Cal();
 });
 
 Resetbtn.addEventListener("click", () => {
+  Startbtn.style.display = "inline";
+  Resumebtn.style.display = "none";
+  Pausebtn.style.display = "none";
   stopinterval();
   sec = 0;
   min = 0;
@@ -53,6 +63,9 @@ Resetbtn.addEventListener("click", () => {
   Min.innerHTML = min > 9 ? min : `0${min}`;
   Hrs.innerHTML = hrs > 9 ? hrs : `0${hrs}`;
 });
+
 Pausebtn.addEventListener("click", () => {
+  Resumebtn.style.display = "inline";
+  Pausebtn.style.display = "none";
   stopinterval();
 });
