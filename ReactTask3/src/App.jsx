3ba5821a4cf5fdbacfda2058f8e5001092c2697f
Shitaalrai <raichichi50@gmail.com>
2026-09-19 20:05:19 +0545

    Practice of Usestate at home

--- ReactTask3/src/App.jsx
+++ ReactTask3/src/App.jsx
@@ -1,32 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import Buttons from './components/Buttons';
+import React, { useEffect, useState } from "react";
+import Buttons from "./components/Buttons";
 
 function App() {
-
-  const [count,setcount] = useState(0);
-  const increment =() =>{
+  const [count, setcount] = useState(0);
+  const increment = () => {
     setcount(count + 1);
-  }
- useEffect(() => {
+  };
+  useEffect(() => {
     console.log(count);
-    }, [count]);
+  }, [count]);
 
-  const decrement =() =>{
+  const decrement = () => {
     setcount(count - 1);
-  }
-  const value= (e)=>{
-    console.log(e.target.value);
-    
+  };
+
+  const [inputData, setInputData] = useState();
+
+  const changetext = (e) => {
+    setInputData(e.target.value);
+  };
+
+  function clearText() {
+    setInputData("");
   }
 
   return (
-    <div className='flex gap-4 '>
-      <Buttons onClick={increment} text={"+"}/>
-      <h1>{count}</h1>
-      <Buttons onClick={decrement} text={"-"}/>
-      <input type='text' onChange={value} placeholder='Name'/>
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
+      <div className="flex gap-6">
+        <Buttons onClick={increment} text={"+"} />
+        <h1>{count}</h1>
+        <Buttons onClick={decrement} text={"-"} />
+      </div>
+      <hr />
+      <div className="flex flex-col gap-4 border-6 p-6 items-center justify-center">
+        <h1 className="text-4xl font-bold">Enter anything</h1>
+        <textarea
+          value={inputData}
+          type="text"
+          onChange={changetext}
+          placeholder="Name"
+          className="border-2  w-60"
+        />
+        <h2 className="text-xl text-red-600">{inputData}</h2>
+        <button className="border-4 rounded-md w-20" onClick={clearText}>
+          Clear
+        </button>
+      </div>
     </div>
-
-  )
+  );
 }
-export default App
+export default App;
